3ce477eb233facb617bf886b23a288ffc11a1c86
whx752 <978683483@qq.com>
2024-02-23 00:11:23 +0800

    加密解密

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -1,10 +1,6 @@
-from datetime import datetime
-from flask import render_template, request
 from run import app
-from wxcloudrun.dao import delete_counterbyid, query_counterbyid, insert_counter, update_counterbyid
-from wxcloudrun.model import Counters
-from wxcloudrun.response import make_succ_empty_response, make_succ_response, make_err_response
-
+from flask import request, json, render_template
+import cryptocode
 
 @app.route('/')
 def index():
@@ -14,53 +10,38 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/api/count', methods=['POST'])
-def count():
-    """
-    :return:计数结果/清除结果
-    """
+@app.route('/api/encrypt', methods=['POST'])
+def api_encrypt():
+    # 默认返回内容
+    return_dict = {'return_code':'200','return_info':'处理成功','result':None}
+    # 判断传入的json数据是否为空
+    if len(request.get_data()) == 0:
+        return_dict['return_code'] = '5004'
+        return_dict['return_info'] = '请求参数为空'
+        return json.dumps(return_dict, ensure_ascii=False)
+    miyao = request.json.get('miyao')
+    message = request.json.get('message')
+    miwen = cryptocode.encrypt(message,miyao)
 
-    # 获取请求体参数
-    params = request.get_json()
+    if len(miwen) != 0:
+        return_dict = {'return_code':'200','return_info':'处理成功','result':miwen}
+    print(miwen)
+    return return_dict
 
-    # 检查action参数
-    if 'action' not in params:
-        return make_err_response('缺少action参数')
+@app.route('/api/decrypt', methods=['POST'])
+def api_decrypt():
+    # 默认返回内容
+    return_dict = {'return_code':'200','return_info':'处理成功','result':None}
+    # 判断传入的json数据是否为空
+    if len(request.get_data()) == 0:
+        return_dict['return_code'] = '5004'
+        return_dict['return_info'] = '请求参数为空'
+        return json.dumps(return_dict, ensure_ascii=False)
+    miyao = request.json.get('miyao')
+    message = request.json.get('message')
+    miwen = cryptocode.decrypt(message,miyao)
 
-    # 按照不同的action的值，进行不同的操作
-    action = params['action']
-
-    # 执行自增操作
-    if action == 'inc':
-        counter = query_counterbyid(1)
-        if counter is None:
-            counter = Counters()
-            counter.id = 1
-            counter.count = 1
-            counter.created_at = datetime.now()
-            counter.updated_at = datetime.now()
-            insert_counter(counter)
-        else:
-            counter.id = 1
-            counter.count += 1
-            counter.updated_at = datetime.now()
-            update_counterbyid(counter)
-        return make_succ_response(counter.count)
-
-    # 执行清0操作
-    elif action == 'clear':
-        delete_counterbyid(1)
-        return make_succ_empty_response()
-
-    # action参数错误
-    else:
-        return make_err_response('action参数错误')
-
-
-@app.route('/api/count', methods=['GET'])
-def get_count():
-    """
-    :return: 计数的值
-    """
-    counter = Counters.query.filter(Counters.id == 1).first()
-    return make_succ_response(0) if counter is None else make_succ_response(counter.count)
+    if len(miwen) != 0:
+        return_dict = {'return_code':'200','return_info':'处理成功','result':miwen}
+    print(miwen)
+    return return_dict
